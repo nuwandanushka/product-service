c043f1dde01ab4ea10967dcb175f90ea3adf74ba
Nuwan <nuwan.wickramasooriya@theoptimum.tech>
2024-01-23 10:12:10 +0800

    implementation of product-service and unit test

--- src/test/java/com/demo/productservice/BaseProductServiceApplicationTest.java
+++ src/test/java/com/demo/productservice/BaseProductServiceApplicationTest.java
@@ -1,11 +1,13 @@
 package com.demo.productservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 public class BaseProductServiceApplicationTest {
 
 	@Test
--- src/test/java/com/demo/productservice/controller/ProductControllerTest.java
+++ src/test/java/com/demo/productservice/controller/ProductControllerTest.java
@@ -22,7 +22,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
 public class ProductControllerTest extends BaseProductServiceApplicationTest {
 
     @Autowired
