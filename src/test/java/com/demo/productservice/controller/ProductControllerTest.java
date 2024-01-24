fcf513ed95a93af3f4b3cbfc08250c9924306fc8
Nuwan <nuwan.wickramasooriya@theoptimum.tech>
2024-01-24 12:44:50 +0800

    implementation of product-service and unit test

--- src/test/java/com/demo/productservice/controller/ProductControllerTest.java
+++ src/test/java/com/demo/productservice/controller/ProductControllerTest.java
@@ -1,6 +1,5 @@
 package com.demo.productservice.controller;
 
-import com.demo.productservice.BaseProductServiceApplicationTest;
 import com.demo.productservice.dto.CreateUpdateProductRequest;
 import com.demo.productservice.dto.ProductResponse;
 import com.demo.productservice.service.ProductService;
@@ -8,8 +7,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -22,7 +23,10 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class ProductControllerTest extends BaseProductServiceApplicationTest {
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class ProductControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
